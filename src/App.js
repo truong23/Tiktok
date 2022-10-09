3029aa51b1e0736b0ad49284a914b764560df2a2
truong23 <truongdv.dev@gmail.com>
2022-10-09 16:45:42 +0700

    add pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import './App.css'; 
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaultLayout
+            if (route.layout) {
+                Layout = route.layout
+            } else if (route.layout === null) {
+                Layout = Fragment
+            }
+            const Page = route.component
+            return <Route 
+                        key={index} 
+                        path={route.path} 
+                        element={
+                          <Layout>
+                              <Page />
+                          </Layout>
+                        } />
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
